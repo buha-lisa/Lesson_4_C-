b8bfd33984d213014ac0051826f50c7c7f7b118d
buha-lisa <bugaliza77@gmail.com>
2024-03-03 16:02:11 +0200

    Add project files.

---
+++ cs4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace cs4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the task(1-6):  ");
+            int.TryParse(Console.ReadLine(), out int task);
+            switch (task)
+            {
+                case 1:
+                    labFour.Task1();
+                    break;
+            }
+        }
+    }
+}
---
+++ cs4/labFour.cs
@@ -0,0 +1,29 @@
+﻿namespace cs4
+{
+    internal class labFour
+    {
+
+        public static void Task1()
+        {
+            Console.WriteLine("Enter a string with number(0-9):");
+            string str = Console.ReadLine();
+            try
+            {
+                if (str.Length > int.MaxValue)
+                {
+                    throw new Exception("Error..");
+                }
+                int.TryParse(str, out int num);
+                Console.WriteLine(num);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            { 
+                Console.WriteLine("End..."); 
+            }
+        }
+    }
+}
